427f683aa7abeb546a7d3e6ff412d8a2162de0d7
Steven Quispe <steven.quispe@epn.edu.ec>
2024-05-20 22:09:13 -0500

    feat: Añadir librería de estilos general

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TemplateComponent } from './shared/layouts/template/template.component';
-import { QuestionnarieFormComponent } from './modules/questionnaires/components/questionnarie-form/questionnarie-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterOutlet, TemplateComponent, QuestionnarieFormComponent],
+  imports: [ReactiveFormsModule, RouterOutlet, TemplateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
